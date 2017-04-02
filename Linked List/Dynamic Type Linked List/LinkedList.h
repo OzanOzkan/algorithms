0346fb7825067e2a73f3b047a730f6ab1dcaa296
OzanOzkan <ozanozkan@gmail.com>
2017-04-02 14:52:51 +0300

    Generic Type LinkedList Improvements
    with PushFront and PushBack

--- Linked List/Dynamic Type Linked List/LinkedList.h
+++ Linked List/Dynamic Type Linked List/LinkedList.h
@@ -17,12 +17,48 @@ public:
 	{
 	}
 
-	bool AddValue(T value)
+	bool PushFront(T value)
 	{
 		Node<T> *node = new Node<T>();
+
 		node->value = value;
-		node->next = m_head;
+
+		if (m_head != nullptr)
+		{
+			node->next = m_head;
+		}
+
 		m_head = node;
+		m_length++;
+
+		return true;
+	}
+
+	bool PushBack(T value)
+	{
+		Node<T> *node = new Node<T>();
+
+		node->value = value;
+		node->next = nullptr;
+
+		if(m_head != nullptr)
+		{
+			Node<T> *currentNode = m_head;
+
+			do
+			{
+				if (currentNode->next == nullptr)
+				{
+					currentNode->next = node;
+					break;
+				}
+
+			} while ((currentNode = currentNode->next) != nullptr);
+		}
+		else
+		{
+			m_head = node;
+		}
 
 		m_length++;
 
@@ -31,28 +67,22 @@ public:
 
 	T GetValue(int index)
 	{
-		Node<T>* current = nullptr;
+		Node<T>* current = m_head;
 		int indexCount = 0;
 
-		if (m_head != nullptr)
+		if (m_length > 0)
 		{
-			do {
-
-				if (index == 0)
-				{
-					return m_head->value;
-				}
-				else if (indexCount > index)
+			do
+			{
+				if (indexCount == index)
 				{
 					return current->value;
 				}
 
 				indexCount++;
 
-			} while ((current = m_head->next) != nullptr);
+			} while ((current = current->next) != nullptr);
 		}
-
-		return 0;
 	}
 
 	int GetLength()
