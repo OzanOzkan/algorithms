ec73d585c96d589075336b7de30b1a42dcdacb87
OzanOzkan <ozanozkan@gmail.com>
2017-04-09 15:51:10 +0300

    Update

--- Itoa/itostr.h
+++ Itoa/itostr.h
@@ -13,39 +13,39 @@ public:
 
 	void itostr(int number, char* returnVal)
 	{
-		int reminder;
+		int remainder;
 		int i = 0;
 
 		while (number != 0)
 		{
-			reminder = number % 10;
-			if (reminder > 9)
-				returnVal[i++] = (reminder - 10) + 'a';
+			remainder = number % 10;
+			if (remainder > 9)
+				returnVal[i++] = (remainder - 10) + 'a';
 			else
-				returnVal[i++] = reminder + '0';
+				returnVal[i++] = remainder + '0';
 			;
 			number = number / 10;
 		}
 
 		returnVal[i] = '\0';
 
-		reverse(returnVal, i);
+		reverse(&returnVal, i);
 	}
 
 private:
-	void reverse(char input[], int length)
+	void reverse(char** input, int length)
 	{
 		int i = 0;
 		char tempVal[100];
 		
 		while (i < length)
 		{
-			tempVal[i] = input[(length-1) - i];
+			tempVal[i] = *input[(length-1) - i];
 			i++;
 		}
 
 		tempVal[i] = '\0';
 
-		input = tempVal;
+		*input = tempVal;
 	}
 };
