9319ec4de4670ae324b59394ab416a47d69b3349
OzanOzkan <ozanozkan@gmail.com>
2017-07-20 16:09:04 +0300

    Update MyComponent.h

--- CompositePattern/MyComponent.h
+++ CompositePattern/MyComponent.h
@@ -7,15 +7,11 @@
 class MyComponent
 {
 public:
-	// Pure virtuals with =0 or {} ?
-
-	// For Composite implementation
 	virtual void add(std::unique_ptr<MyComponent>) = 0;
 	virtual void remove(const int id) = 0;
 	virtual MyComponent* getComponent(int componentIndex) const = 0;
 	virtual void getComponentInfo() = 0;
-
-	// For leaf implementation
+	
 	virtual std::string getName() const = 0;
 	virtual int getId() const = 0;
 	virtual void setName(std::string &&name) = 0;
